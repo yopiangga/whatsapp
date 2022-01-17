0c57aa38bdf1f0d08e155719371e4cd161563d41
@yopigambyok <yopigambyok@gmail.com>
2022-01-17 15:40:01 +0700

    create template widget

---
+++ lib/widget/WCallOverview.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WCallOverview extends StatelessWidget {
+  const WCallOverview({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/widget/WChatOverview.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WChatOverview extends StatelessWidget {
+  const WChatOverview({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
